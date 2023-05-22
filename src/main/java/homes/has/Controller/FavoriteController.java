5f227a5d1a999ad22d2c742a4682b408162e4d90
bomsprn <72404724+bomsprn@users.noreply.github.com>
2023-05-22 20:37:45 +0900

    관심건물 컨트롤러 코드

--- src/main/java/homes/has/Controller/FavoriteController.java
+++ src/main/java/homes/has/Controller/FavoriteController.java
@@ -2,14 +2,28 @@ package homes.has.Controller;
 
 import homes.has.service.FavoriteService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/map/favorite")
 @RequiredArgsConstructor
 public class FavoriteController{
     private final FavoriteService favoriteService;
+
+    /**
+     * 관심 건물 등록 (API no.8)
+     **/
+    @PostMapping
+    public void createFavorite(@RequestParam String location, @RequestParam Long memberId) {
+        favoriteService.CreateFavorite(location, memberId);
+    }
+
+    /**
+     * 관심 건물 삭제 (API no.9)
+     **/
+    @DeleteMapping
+    public void deleteFavorite(@RequestParam String location, @RequestParam Long memberId) {
+        favoriteService.DeleteFavorite(location, memberId);
+    }
 
 }
